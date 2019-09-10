1f1e2ea08ebd07928c0572b8d1a8a3d7621cc63c
Dorian Burihabwa <dorian@burihabwa.com>
2019-09-10 10:02:28 +0200

    Tidying up

--- src/minerva-safefs-layer/utils.hpp
+++ src/minerva-safefs-layer/utils.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include <codewrapper/codewrapper.hpp>
+
+/**
+ * A structure representing the folder organization of the minervafs backend folder
+ */
+struct minerva_hierarchy {
+    std::string root_folder;
+    std::string basis_folder;
+    std::string temp_folder;
+    std::string registry_folder;
+    std::string identifier_register;
+    std::string minervafs_config;
+    std::vector<std::string> to_be_ignored;
+};
 
 /**
  * Returns a text representation of coding parameters
