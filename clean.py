07f610e88e0abbd6ee06c51f65223f8410c350e4
Dorian Burihabwa <dorian@burihabwa.com>
2019-09-10 16:12:15 +0200

    Load the config file better in clean.py

--- clean.py
+++ clean.py
@@ -7,18 +7,24 @@ import json
 import os
 import shutil
 
+__DEFAULT_CONFIG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "minervafs.json")
+
 
 def main():
     """
     Main routine
     """
     parser = argparse.ArgumentParser(__file__, "Clears the backend minervafs folder")
-    parser.add_argument("-p", "--path", type=str, help="Path of the directory to delete")
+    parser.add_argument("-p", "--path", type=str,
+                        help="Path of the directory to delete (takes precedence)")
+    parser.add_argument("-c", "--config-file", type=str,
+                        help="Path of the configuration file where the path is located",
+                        default=__DEFAULT_CONFIG_PATH)
     args = parser.parse_args()
     if args.path:
         path = args.path
     else:
-        with open("minervafs.json") as handle:
+        with open(args.config_file) as handle:
             configuration = json.load(handle)
         path = configuration.get("root_folder", "~/.minervafs")
     path = os.path.expanduser(path)
