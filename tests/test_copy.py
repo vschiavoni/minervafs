46e8802660e3aa4dc5d92a9662f29c74f45c6962
Dorian Burihabwa <dorian@burihabwa.com>
2020-04-12 20:23:00 +0200

    Refine tests

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -5,13 +5,23 @@ Basic functional tests for minervafs.
 import filecmp
 import os
 import shutil
+import subprocess
 import time
+
+import pytest
 
 __WORKSPACE = "./test_workspace"
 __MOUNT_POINT = "./test_mountpoint/"
+__DEFAULT_FILE_SIZE_IN_BYTES = 4096
+
 
 def build():
-    os.system("python3 ../waf build")
+    """
+    Compile minervafs code.
+    """
+    command = "python3 ../waf build".split()
+    subprocess.run(command, check=True)
+
 
 def mount(directory):
     """
@@ -20,11 +30,15 @@ def mount(directory):
         directory(str): The directory used as the mountpoint
     """
     if not os.path.exists(directory):
-        os.makedirs(directory)
+        os.makedirs(directory, exist_ok=True)
     elif not os.path.isdir(directory):
         raise ValueError("directory argument is not an actual directory {:s}".format(directory))
-    os.system("python3 ../clear.py")
-    os.system("../build/examples/minervafs-example/minervafs_example {:s}".format(directory))
+    subprocess.run("python3 ../clear.py".split(), check=True)
+    subprocess.run("../build/examples/minervafs-example/minervafs_example {:s}".format(directory).split(), check=True)
+    while not os.path.ismount(directory):
+        time.sleep(1)
+    time.sleep(1)
+
 
 def umount(directory):
     """
@@ -34,27 +48,44 @@ def umount(directory):
     """
     if not os.path.isdir(directory):
         raise ValueError("directory argument is not an actual directory {:s}".format(directory))
-    os.system("fusermount3 -u {:s}".format(directory))
+    if os.path.ismount(directory):
+        subprocess.run("fusermount3 -u {:s}".format(directory).split(), check=True)
 
-def setup_module(module):
+
+def setup_module():
     build()
     os.makedirs(__WORKSPACE, exist_ok=True)
+    os.makedirs(__MOUNT_POINT, exist_ok=True)
+    subprocess.run("fusermount3 -u {:s}".format(__MOUNT_POINT).split(), check=False)
+
+
+def teardown_module():
+    shutil.rmtree(__WORKSPACE)
+    umount(__MOUNT_POINT)
+    shutil.rmtree(__MOUNT_POINT)
+
+
+@pytest.fixture(scope="function")
+def before_every_test(request):
     mount(__MOUNT_POINT)
 
-def teardown_module(module):
-    umount(__MOUNT_POINT)
+    def fin():
+        umount(__MOUNT_POINT)
+    request.addfinalizer(fin)
 
-def test_copy_one_file_out_in():
-    data = os.urandom(16)
+
+def test_copy_one_file_out_in(before_every_test):
+    data = os.urandom(__DEFAULT_FILE_SIZE_IN_BYTES)
     src = os.path.join(__WORKSPACE, "16B.data")
-    with open(src, "wb") as handle:        
+    with open(src, "wb") as handle:
         handle.write(data)
     dst = os.path.join(__MOUNT_POINT, os.path.basename(src))
     shutil.copyfile(src, dst)
     assert filecmp.cmp(src, dst, shallow=False)
 
-def test_copy_one_file_in_in():
-    data = os.urandom(16)
+
+def test_copy_one_file_in_in(before_every_test):
+    data = os.urandom(__DEFAULT_FILE_SIZE_IN_BYTES)
     src = os.path.join(__MOUNT_POINT, "16B.data")
     with open(src, "wb") as handle:
         handle.write(data)
@@ -62,8 +93,9 @@ def test_copy_one_file_in_in():
     shutil.copyfile(src, dst)
     assert filecmp.cmp(src, dst, shallow=False)
 
-def test_copy_one_file_in_out():
-    data = os.urandom(16)
+
+def test_copy_one_file_in_out(before_every_test):
+    data = os.urandom(__DEFAULT_FILE_SIZE_IN_BYTES)
     src = os.path.join(__MOUNT_POINT, "16B.data")
     with open(src, "wb") as handle:
         handle.write(data)
