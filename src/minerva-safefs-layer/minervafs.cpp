7d50611deae47258c1db0656826986047e634cee
Lars Nielsen <lnc13.lars@gmail.com>
2021-03-12 17:34:03 +0100

    [:hammer:] working on path update

--- src/minerva-safefs-layer/minervafs.cpp
+++ src/minerva-safefs-layer/minervafs.cpp
@@ -6,6 +6,14 @@
 
 #include <fuse3/fuse.h>
 
+#include <iostream> // Only for test
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+#include <filesystem>
+
+
 static struct fuse_operations minerva_operations;
 
 int main(int argc, char* argv[])
@@ -33,6 +41,75 @@ int main(int argc, char* argv[])
     minerva_operations.unlink = minerva_unlink;
     minerva_operations.utimens = minerva_utimens;
     minerva_operations.listxattr = minerva_listxattr;
+    
+    std::vector<std::string> args(argc);
 
-    return fuse_main(argc, argv, &minerva_operations, NULL);
+    for (size_t index = 0; index < static_cast<size_t>(argc); ++index)
+    {
+        args[index] = (std::string(argv[index]));
+    }
+
+    for (const auto& elm : args)
+    {
+        std::cout << elm << "\n"; 
+    }
+
+    std::cout << "\n\n";
+
+    auto arg_itr = std::find(std::begin(args), std::end(args), "-cfg");
+
+    if (arg_itr != std::end(args) && (arg_itr + 1) != std::end(args))
+    {
+        auto config_file_path = *(arg_itr + 1);
+        std::cout << "config: " <<  config_file_path << "\n";
+
+        if (!std::filesystem::exists(config_file_path))
+        {
+            std::cout << "Config file does not exists" << std::endl; 
+        }
+        else
+        {
+            std::cout << "a:" << std::filesystem::path(config_file_path).parent_path() <<"\n"; 
+        }
+            
+
+        int new_argc = argc - 2; 
+        char* new_argv[new_argc]; 
+        
+        size_t cfg_index = 0;
+        size_t new_args_index = 0;
+        for (size_t index = 0; index < args.size(); ++index)
+        {
+            if (args[index] == "-cfg")
+            {
+                cfg_index = index;
+                std::cout << "cfg: " << cfg_index << "\n";
+            } else if (index != cfg_index && index != (cfg_index + 1))
+            {
+                new_argv[new_args_index] = argv[index];
+                new_args_index = new_args_index + 1;
+                std::cout << new_args_index << "\n\n";
+            } 
+        }
+
+        std::cout << new_argc << "\n";
+        for (int i = 0; i < new_argc; i++)
+        {
+            std::cout << std::string(new_argv[i]) << "\n";
+        }
+
+        return fuse_main(new_argc, new_argv, &minerva_operations, NULL);        
+    }
+    else
+    {
+        return fuse_main(argc, argv, &minerva_operations, NULL);
+        // 
+    }
+    
+
+    
+
+    std::cout << "a\n";
+
+
 }
